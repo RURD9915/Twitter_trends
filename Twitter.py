f6c31704fe255b404f671594264f69beb9e75c81
RURD9915 <rurd9915@gmail.com>
2025-01-16 04:59:33 +0530

    q

--- Twitter.py
+++ Twitter.py
@@ -51,7 +51,7 @@ def fetch_trends():
 
     # Initialize WebDriver without proxy
     try:
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(options=chrome_options)
     except Exception as e:
         print(f"Error initializing WebDriver: {e}")
         sys.exit(10)
